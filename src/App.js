7d1dbe0abe960dba66b3ad1093d101b7dc525b45
MiPhat <phannguyenminhphat1@gmail.com>
2023-11-10 13:37:47 +0700

    update master add redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import Alo from "./BaiTapXemTRuoc/alo";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App container"></div>;
 }
 
 export default App;
